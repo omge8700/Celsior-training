7d00a49923f9298d140680c1a43da2a11dda38b5
Om Singh <102544386+omge8700@users.noreply.github.com>
2024-10-21 20:55:46 +0530

    EVent Booking

---
+++ EventBooking/Repositories/EventRepository.cs
@@ -0,0 +1,46 @@
+﻿using EventBooking.Context;
+using EventBooking.Interface;
+using EventBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using static EventBooking.Controllers.EventController;
+
+
+namespace EventBooking.Repositories
+{
+    public class EventRepository : IEventRepository, IRepository<Event>
+    {
+        private readonly CorporateEventContext _context;
+
+        public EventRepository(CorporateEventContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Event entity)
+        {
+           return await _context.Events
+                .Include(e => e.Event)
+
+        }
+
+        public async Task<IEnumerable<Event>> GetAllAsync()
+        {
+            var evee = await _context.Events.ToListAsync();
+            return evee;
+        }
+
+        public async Task<Event> GetByIdAsync(int id)
+        {
+            return await _context.Events.FindAsync(id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
