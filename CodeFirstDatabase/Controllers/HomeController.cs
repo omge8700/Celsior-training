e941968140fdde2b9f0ed01b4fc68f4a27661343
Om Singh <102544386+omge8700@users.noreply.github.com>
2024-10-22 00:40:07 +0530

    CodeFirstDatabase MVC solution

---
+++ CodeFirstDatabase/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using CodeFirstDatabase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
+
+namespace CodeFirstDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StudentDBContext studentDb;
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController(StudentDBContext studentDb)
+        {
+            this.studentDb = studentDb;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var stdData = await studentDb.Students.ToListAsync();
+            return View(stdData);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult >Create(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                await studentDb.Students.AddAsync(std);
+                await studentDb.SaveChangesAsync();
+                TempData["insert_success"] = "Inserted..";
+                return RedirectToAction("Index", "Home");
+            }
+            return View(std);
+        }
+
+        public async Task<IActionResult> Details(int ? id)
+        {
+            if(id==null ||studentDb.Students ==null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDb.Students.FirstOrDefaultAsync(x=>x.Id==id);
+
+            if(stdData== null)
+            {
+                return NotFound();
+            }
+
+            return View(stdData);
+        }
+
+        public async Task<IActionResult> Edit(int ? id)
+        {
+            if(id== null || studentDb.Students == null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDb.Students.FindAsync(id);
+            if (stdData == null)
+            {
+
+                return NotFound();
+
+            }
+            return View(stdData);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, Student std)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                studentDb.Students.Update(std);
+                await studentDb.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(std);
+
+       }
+
+        public async Task<IActionResult> Delete(int ? id)
+        {
+            if (id == null || studentDb.Students == null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDb.Students.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (stdData == null)
+            {
+
+                return NotFound();
+
+            }
+
+            return View(stdData);
+        }
+
+        [HttpPost,ActionName("Delete")]
+
+        public async Task<IActionResult>DeleteConfirmed(int ? id)
+        {
+            var stdData = await studentDb.Students.FindAsync(id);
+            if(stdData !=null)
+            {
+                studentDb.Students.Remove(stdData);
+            }
+            await studentDb.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CodeFirstDatabase/Migrations/20241019172934_CodeFirstAddClass.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstDatabase.Migrations
+{
+    public partial class CodeFirstAddClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Students",
+                newName: "Standard");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Standard",
+                table: "Students",
+                newName: "MyProperty");
+        }
+    }
+}
