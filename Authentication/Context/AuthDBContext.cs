690566a84e05dc9f64cde703eb765cf32583eb9d
Om Singh <102544386+omge8700@users.noreply.github.com>
2024-11-03 23:53:58 +0530

    Authentication from youtube

---
+++ Authentication/Context/AuthDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Context
+{
+    public class AuthDBContext : IdentityDbContext
+
+    {
+        public AuthDBContext(DbContextOptions options) : base(options)
+        {
+
+            
+        }
+
+
+    }
+}
